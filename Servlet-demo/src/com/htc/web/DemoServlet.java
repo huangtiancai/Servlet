54947772c6dccbdd981b5ebead66a6463556c00e
huangtiancai <1812869775@qq.com>
2019-04-05 18:21:44 +0800

    commmit

--- Servlet-demo/src/com/htc/web/DemoServlet.java
+++ Servlet-demo/src/com/htc/web/DemoServlet.java
@@ -19,6 +19,7 @@ public class DemoServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet...");
+		System.out.println("-------");
 	}
 	
 	@Override
