b06bc92e4d0d8bf7f26653ac1fd85f9664a22408
huangtiancai <1812869775@qq.com>
2019-04-09 00:13:12 +0800

    提交

--- Servlet-demo/src/com/htc/web/DemoServlet.java
+++ Servlet-demo/src/com/htc/web/DemoServlet.java
@@ -3,23 +3,21 @@ package com.htc.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-//Interface Servlet
-//ʵ��Servlet�ӿڣ���дһ����չjavax.servlet.http.HttpServlet �� HTTP servlet
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author htc
- *	Servlet�����Ĳ���
- *	����һ���� �̳� HttpServlet ��
+ *	
+ *
  */
 public class DemoServlet extends HttpServlet{
-	//��дdoGet��doPost����
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doGet...");
-		System.out.println("���ԣ�");
+		System.out.println("测试");
 	}
 	
 	@Override
