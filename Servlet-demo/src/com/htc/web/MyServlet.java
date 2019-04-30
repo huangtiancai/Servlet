5a20eceb9b3d5713ad3d4a900ce4160276704801
huangtiancai <1812869775@qq.com>
2019-04-30 17:37:22 +0800

    需求：获取注册页面的值-使用request对象(根据name属性)

--- Servlet-demo/src/com/htc/web/MyServlet.java
+++ Servlet-demo/src/com/htc/web/MyServlet.java
@@ -1,6 +1,8 @@
 package com.htc.web;
 
 import java.io.IOException;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,26 @@ public class MyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("MyServlet.....doGet..........");
+		//需求：获取注册页面的值-使用request对象(根据name属性)
+		String username = request.getParameter("username");
+		System.out.println("用户名："+username);
+		
+		System.out.println("-------1-------");
+		String[] values = request.getParameterValues("hobby");
+		for(String value:values){
+			System.out.println("value:"+value);
+		}
+		
+		System.out.println("-------2-------");
+		Map<String, String[]> map = request.getParameterMap();
+		System.out.println("map:"+map);
+		for(String key : map.keySet()){
+			for(String value : map.get(key)){
+				System.out.println("value:"+value);
+			}
+		}
+		
+		
 	}
 
 
