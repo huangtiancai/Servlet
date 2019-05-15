5862daaf727608a701c4d55941aa12d4dfca3fb0
huangtiancai <1812869775@qq.com>
2019-05-15 17:18:01 +0800

    提交

--- Servlet-demo/src/com/htc/web/MyServlet.java
+++ Servlet-demo/src/com/htc/web/MyServlet.java
@@ -16,20 +16,25 @@ public class MyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("MyServlet.....doGet..........");
+		System.out.println("-------1-------");
 		//需求：获取注册页面的值-使用request对象(根据name属性)
+		//String getParameter(String name)  根据name 获取对应的值
 		String username = request.getParameter("username");
 		System.out.println("用户名："+username);
 		
-		System.out.println("-------1-------");
+		System.out.println("-------2-------");
+		//String[] getParameterValues(String name)  获取name相同的所有value,如复选框
 		String[] values = request.getParameterValues("hobby");
 		for(String value:values){
 			System.out.println("value:"+value);
 		}
-		
-		System.out.println("-------2-------");
-		Map<String, String[]> map = request.getParameterMap();
-		System.out.println("map:"+map);
+		//Map getParameterMap 参数名作为key，参数值作为value，封装到map中
+		System.out.println("-------3-------");
+		Map<String, String[]> map = request.getParameterMap();	
 		for(String key : map.keySet()){
+			System.out.println("key:"+key);
+			System.out.println("map:"+map);
+			System.out.println("map.keySet():"+map.keySet());
 			for(String value : map.get(key)){
 				System.out.println("value:"+value);
 			}
