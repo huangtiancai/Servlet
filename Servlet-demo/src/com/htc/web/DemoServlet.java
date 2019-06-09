d7faf25db59e83d282656624a911a4e7c5c371ae
1812869775@qq.com <1812869775@qq.com>
2019-06-09 18:16:05 +0800

    DemoServlet-update

--- Servlet-demo/src/com/htc/web/DemoServlet.java
+++ Servlet-demo/src/com/htc/web/DemoServlet.java
@@ -9,9 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet{
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init");
+		super.init();
+	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//第一种写法：
 		System.out.println("doGet...");
 	}
 	
@@ -19,4 +24,30 @@ public class DemoServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("doPost...");
 	}	
+	
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("service");
+		super.service(req, resp);
+	}
+	
+	
+	@Override
+	public void destroy() {
+		System.out.println("destory");
+		super.destroy();
+	}
+	
+	//在整个servlet的生命周期内，init方法只被调用了一次,后续访问demoServlet只会执行doGet方法
+//	init
+//	doGet...
+//	doGet...
+//	doGet...
+//	...
+//	六月 09, 2019 4:23:58 下午 org.apache.catalina.core.ApplicationContext log
+//	信息: ContextListener: contextDestroyed()
+//	destory
+//	六月 09, 2019 4:23:59 下午 org.apache.coyote.AbstractProtocol stop
+//	...
 }
