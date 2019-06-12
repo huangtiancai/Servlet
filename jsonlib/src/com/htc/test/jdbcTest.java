38317a13b00dd4883f7c6f8ec14f225e76d541ff
huangtiancai <1812869775@qq.com>
2019-06-12 22:14:33 +0800

    jar包添加+xml+测试JDBC连接

---
+++ jsonlib/src/com/htc/test/jdbcTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.htc.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.htc.utils.C3P0Utils;
+
+/**  
+* @ClassName:jdbcTest
+* @Description:测试JDBC连接
+* @author:htc  
+* @date:2019年6月12日 下午10:11:01
+*/
+public class jdbcTest {
+	public void testJdbc(){
+		try {
+			Connection conn = C3P0Utils.getConnection();
+			System.out.println(conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ jsonlib/src/com/htc/utils/C3P0Utils.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.htc.utils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/**  
+* @ClassName:C3P0Util
+* @Description:c3p0连接池--工具类(等效 jdbc-demo中的JdbcUtils，用于提供获得连接工具类)
+* @author:htc  
+* @date:2019年6月2日 下午10:06:37
+*/
+public class C3P0Utils {
+	//C3P0提供核心工具类：ComboPooledDataSource，如果要使用连接池，必须创建该类的实例对象
+	//new ComboPooledDataSource(“名称”); 使用配置文件“命名配置”  <named-config name="htcconfig">
+	//new ComboPooledDataSource(); 使用配置文件“默认配置”       <default-config>
+	private static ComboPooledDataSource dataSource = new ComboPooledDataSource("htcconfig");
+	
+	/**
+	 * function: getDataSource
+	 * Description:获得数据源（连接池）  数据源=连接池
+	 * return:DataSource
+	 */
+	public static DataSource getDataSource(){
+		return dataSource;
+	}
+	/**
+	 * function: getConnection
+	 * Description:获得连接
+	 * return:Connection
+	 */
+	public static Connection getConnection() throws SQLException{
+		//将从连接池中获得连接
+		return dataSource.getConnection();
+	}
+	
+	
+	
+	
+	
+}
