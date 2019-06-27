f3e6a1d9b4da63f8b83cc46321ebdad9e9755b9d
1812869775@qq.com <1812869775@qq.com>
2019-06-27 15:19:58 +0800

    DemoServlet1

--- Servlet-demo/src/com/htc/web/DemoServlet1.java
+++ Servlet-demo/src/com/htc/web/DemoServlet1.java
@@ -10,7 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 public class DemoServlet1 extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("进入demoservlet1");
+		//第一种写法：url直接进入Servlet
+		//http://localhost:8080/Servlet-demo/demoServlet1(不带参数)
+		System.out.println("进入DemoServlet1");
+		System.out.println("进入doGet...");
+		
+		//直接输入带有参数的url
+		//http://localhost:8080/Servlet-demo/demoServlet1?name=zhangsang&age=18&hobby=ball1&hobby=ball2
+		//获取单个参数值
+		System.out.println("参数name:"+request.getParameter("name"));
+		System.out.println("参数age:"+request.getParameter("age"));
+		//获取同名参数的多个值
+		String hobbys[] = request.getParameterValues("hobby");
+		for(String str : hobbys){
+			System.out.println("hobby:"+str);
+		}
 	}
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
