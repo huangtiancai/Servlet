256e64f9439497cb80ac5c74b7b20860ac1fa8be
huangtiancai <1812869775@qq.com>
2019-09-07 23:22:14 +0800

    add common class

---
+++ CommonClass/src/com/htc/math/MathAction.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.htc.math;
+
+/**
+ * @ClassName MathAction
+ * @Description:
+ * @author htc
+ * @date 2019年9月6日下午4:21:33
+ */
+public class MathAction {
+	public static void main(String[] args) {
+		double a = 10.12;
+		double b = 10.88;
+		double c = -10.45;
+		double d = -10.55;	
+		
+		// 强制转换
+		int m1 = (int) a;
+		int m2 = (int) b;
+ 
+		// ceil
+		double m3 = Math.ceil(a);
+		double m5 = Math.ceil(b);
+		double m6 = Math.ceil(b);
+		
+		System.out.println(c);// 13.0
+
+	}
+}
