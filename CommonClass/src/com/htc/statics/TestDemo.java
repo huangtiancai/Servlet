ee07c232438f084b19e93a7e8bb4e03e7690c808
1812869775@qq.com <1812869775@qq.com>
2019-11-20 17:20:07 +0800

    commit

--- CommonClass/src/com/htc/statics/TestDemo.java
+++ CommonClass/src/com/htc/statics/TestDemo.java
@@ -14,8 +14,10 @@ import org.junit.Test;
 public class TestDemo {
 	@Test
 	public void testCar(){
+		new Car("Hello","T6");
 		new Car("TT", "V8");
 		new Car("WW", "W8");
+		// 对类变量（静态变量）和方法（静态方法）的访问可以直接使用 classname.variablename 和 classname.methodname 的方式访问
 		System.out.println(Car.numberOfCars);
 	}
 }
